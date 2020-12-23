ad9c658368948e7eac38804788fc0bdefc3495bc
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2020-12-23 08:24:02 +0100

    pseudotable added

--- src/pages/Numerology.js
+++ src/pages/Numerology.js
@@ -3,8 +3,14 @@ import allNumerologicsByDate from "../components/numerologicNumbers"
 
 function Numerology() {
     const dates = allNumerologicsByDate(2020, 2020);
+    let table = "";
+    const showDatesInTable = dates.map((element) => {
+        return <p>{element.data} + {element.numerologicNumber}</p>
+    })
 
     return <>
+        {console.log(showDatesInTable)}
+        {showDatesInTable}
         <h2>Numerology Welcome</h2>
     </>
 }
