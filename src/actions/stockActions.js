2b47b8942427ee9ba84228d91c9a05e36e393ad5
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2021-01-11 19:52:58 +0100

    stick with adding

--- src/actions/stockActions.js
+++ src/actions/stockActions.js
@@ -1,7 +1,18 @@
 export const addCompanyToMarket = (marketIndices, companies, index) => {
+  const newMarketIndex = marketIndices.map((el) => {
+    console.log("elIndex", el[index]);
+    if (el[index]) {
+      console.log(el[index].concat(companies));
+      return { el: el[index].concat(companies) };
+    } else {
+      console.log(el);
+      return el;
+    }
+  });
   console.log("into action");
+  console.log("newMarket", newMarketIndex);
   return {
     type: "ADD_COMPANIES_TO_INDEX",
-    marketIndices: marketIndices,
+    marketIndices: newMarketIndex,
   };
 };
